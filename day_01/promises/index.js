0b562d97609afe25bf0ce891ba236e928bc33f89
ronitsharma14 <ronitt.sharma1402@gmail.com>
2026-01-28 15:00:04 +0530

    day 1 promises

---
+++ day_01/promises/index.js
@@ -0,0 +1,22 @@
+const mypromise = new Promise((resolve,reject)=>{
+    let marks = 80;
+    if(marks>33){
+        resolve("pass");
+    }else{
+        reject("fail");
+    }
+
+})
+const myPromise = new promise((resolve,reject)=>{
+    let marks = 80;
+    if(marks>33){
+        resolve("pass");
+    }else{
+        reject("fail");
+    }
+
+})
+
+myPromise.then((msg) =>console.log(msg))
+.catch((error)=>console.log("some error occured."))
+//console.log(myPromise)
